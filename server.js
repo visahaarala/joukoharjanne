9094ac8eacfed5487e5687d9c5d65ac608f51ba6
Visa Haarala <visa.haarala@gmail.com>
2025-07-12 12:53:45 +0300

    path.join(__dirname

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
 const biography = require('./biography');
 
 app.set('view engine', 'pug');
-app.set('views', './views');
+app.set('views', path.join(__dirname, '/views'));
 
 // home
 app.get('/:lang', (req, res) => {
