824261d85f65ec52b21a059cbbd450d9b63a99dc
Visa Haarala <visa.haarala@gmail.com>
2025-07-12 11:30:52 +0300

    trying to do the typescript thing

--- server.ts
+++ server.ts
@@ -1,38 +1,52 @@
 import { Request, Response } from 'express';
+import express from 'express';
+import { translations, languages, suffixes } from './translations';
 
-const express = require('express');
+// const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
 
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, './views'));
-
-const translations = require('./translations');
+// app.set('views', path.join(__dirname, './views'));
+// app.set('views', express.static(__dirname + '/views'));
 
 // home
 app.get('/:lang', (req: Request, res: Response) => {
   const lang = req.params.lang;
-  if (!translations[lang]) return res.redirect('/en');
+  // if (!translations[lang]) return res.redirect('/en');
   res.render('index', {
+    languages,
     lang,
-    title: translations[lang].home,
-    translations,
-    url_suffix: '',
+    translation: translations[lang],
+    pageName: '',
   });
 });
 
-// about
-app.get('/:lang/about', (req: Request, res: Response) => {
-  const lang = req.params.lang;
-  if (!translations[lang]) return res.redirect('/en/about');
-  res.render('about', {
+app.get('/:lang/:suffix', (req: Request, res: Response) => {
+  const { lang, suffix } = req.params;
+  // check sub
+  const pageName = suffixes.includes(suffix) ? suffix : suffixes[0];
+  if (!translations[lang]) return res.redirect('/en/' + pageName);
+  res.render(pageName, {
+    languages,
     lang,
-    title: translations[lang].about,
-    translations,
-    url_suffix: '/about',
+    translation: translations[lang],
+    pageName,
   });
 });
+
+// // about
+// app.get('/:lang/about', (req: Request, res: Response) => {
+//   const lang = req.params.lang;
+//   if (!translations[lang]) return res.redirect('/en/about');
+//   res.render('about', {
+//     lang,
+//     title: translations[lang].about,
+//     translations,
+//     url_suffix: '/about',
+//   });
+// });
 
 // redirect root to english
 app.get('/', (req: Request, res: Response) => res.redirect('/en'));
@@ -42,4 +56,4 @@ app.use((req: Request, res: Response) =>
   res.status(404).send('joukoharjanne.com --- 404 not found')
 );
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- translations.ts
+++ translations.ts
@@ -1,23 +1,65 @@
-module.exports = {
+type Content = {
+  title: string;
+  heading: string;
+  paragraphs: string[];
+};
+
+type Translation = {
+  home: Content;
+  about: Content;
+};
+
+export const languages = ['en', 'fi'];
+
+export const suffixes = ['home', 'about']
+
+export const translations: { [lang: string]: Translation } = {
   en: {
-    home: 'Home',
-    about: 'About',
-    welcome: 'Welcome to the site!',
-    intro: 'This is a simple multilingual site example.',
-    about_msg: 'We are an example website.',
+    home: {
+      title: 'home',
+      heading: 'homeheading',
+      paragraphs: ['homeLorem1', 'homeLorem2'],
+    },
+    about: {
+      title: 'about',
+      heading: 'aboutHeading',
+      paragraphs: ['aboutLorem1', 'aboutLorem2'],
+    },
   },
   fi: {
-    home: 'Etusivu',
-    about: 'Tietoa',
-    welcome: 'Tervetuloa sivustolle!',
-    intro: 'Tämä on yksinkertainen monikielinen sivusto.',
-    about_msg: 'Olemme esimerkkisivusto.',
-  },
-  jp: {
-    home: 'ホーム',
-    about: '紹介',
-    welcome: 'サイトへようこそ！',
-    intro: 'これは簡単な多言語サイトの例です。',
-    about_msg: '私たちは例のウェブサイトです。',
+    home: {
+      title: 'koti',
+      heading: 'kotiheading',
+      paragraphs: ['kotiLorem1', 'kotiLorem2'],
+    },
+    about: {
+      title: 'tietoja',
+      heading: 'tietojaHeading',
+      paragraphs: ['tietojaLorem1', 'tietojaLorem2'],
+    },
   },
 };
+
+// const translations: Translations = {
+//   en: {
+//     home: 'Home',
+//     about: 'About',
+//     welcome: 'Welcome to the site!',
+//     intro: 'This is a simple multilingual site example.',
+//     about_msg: 'We are an example website.',
+//   },
+//   fi: {
+//     home: 'Etusivu',
+//     about: 'Tietoa',
+//     welcome: 'Tervetuloa sivustolle!',
+//     intro: 'Tämä on yksinkertainen monikielinen sivusto.',
+//     about_msg: 'Olemme esimerkkisivusto.',
+//   },
+//   jp: {
+//     home: 'ホーム',
+//     about: '紹介',
+//     welcome: 'サイトへようこそ！',
+//     intro: 'これは簡単な多言語サイトの例です。',
+//     about_msg: '私たちは例のウェブサイトです。',
+//   },
+// };
