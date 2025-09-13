4971421df15425f29520420eca3a6eab41c3876b
Visa Haarala <visa.haarala@gmail.com>
2025-09-13 10:46:51 +0300

    jouko picture

--- biography.js
+++ biography.js
@@ -1,45 +1,54 @@
 module.exports = {
-  en: [
-    'Jouko Harjanne studied the trumpet with Raimo Sarmas at the Tampere Conservatoire and continued after graduating with Henri Adelbrecht and Timofei Dokshitser. Between 1978 and 1984, Harjanne was alternate lead trumpeter with the Tampere Philharmonic Orchestra. 1984 – 2024 Harjanne served as a principal trumpet of the Finnish Radio Symphony Orchestra in Helsinki.',
+  en: {
+    intro: [
+      'Jouko Harjanne studied the trumpet with Raimo Sarmas at the Tampere Conservatoire and continued after graduating with Henri Adelbrecht and Timofei Dokshitser. Between 1978 and 1984, Harjanne was alternate lead trumpeter with the Tampere Philharmonic Orchestra. 1984 – 2024 Harjanne served as a principal trumpet of the Finnish Radio Symphony Orchestra in Helsinki.',
+    ],
+    bio: [
+      'Harjanne has played chamber music in many brass and chamber ensembles, including the Finnish Brass Ensemble and the Brass Septet Imperial.',
 
-    'Harjanne has played chamber music in many brass and chamber ensembles, including the Finnish Brass Ensemble and the Brass Septet Imperial.',
+      'Jouko Harjanne’s international career has been enhanced by many competition successes, the most important being second place in the Prague Spring Trumpet Series in 1987, and first place in the Ellsworth Smith Trumpet Competition, organised by the International Trumpet Guild in 1990. In 1989 he was presented with the Brass Player of the Year award at the Lieksa Brass Week, Finland’s most important brass event. Harjanne is now the artistic director of the festival. He has also been much in demand as a player in international brass events. In 1997 Jouko was nominated as an honorary member of the Finnish Trumpet Guild.',
 
-    'Jouko Harjanne’s international career has been enhanced by many competition successes, the most important being second place in the Prague Spring Trumpet Series in 1987, and first place in the Ellsworth Smith Trumpet Competition, organised by the International Trumpet Guild in 1990. In 1989 he was presented with the Brass Player of the Year award at the Lieksa Brass Week, Finland’s most important brass event. Harjanne is now the artistic director of the festival. He has also been much in demand as a player in international brass events. In 1997 Jouko was nominated as an honorary member of the Finnish Trumpet Guild.',
+      'Harjanne’s solo performances abroad have extended as far as Russia, Japan, Republic of Korea and the USA, as well as Scandinavia and other parts of Europe. He has been a jury member of the numerous international trumpet competitions for example ARD-Munich, Maurice Andre Trumpet Competition, Budapest International Trumpet Competition, Porcia, Prague Spring and naturally Lieksa Brass Week competitions.',
 
-    'Harjanne’s solo performances abroad have extended as far as Russia, Japan, Republic of Korea and the USA, as well as Scandinavia and other parts of Europe. He has been a jury member of the numerous international trumpet competitions for example ARD-Munich, Maurice Andre Trumpet Competition, Budapest International Trumpet Competition, Porcia, Prague Spring and naturally Lieksa Brass Week competitions.',
+      'Harjanne’s career received an additional boost by his performing and recording in England with soprano Dame Kiri Te Kanawa in December 1994, and by his performance, to great success, of Rodion Schedrin’s new trumpet concertos European premiere in Moscow in autumn 1995. The professional music press has ranked Harjanne among the elite class of international soloists.',
 
-    'Harjanne’s career received an additional boost by his performing and recording in England with soprano Dame Kiri Te Kanawa in December 1994, and by his performance, to great success, of Rodion Schedrin’s new trumpet concertos European premiere in Moscow in autumn 1995. The professional music press has ranked Harjanne among the elite class of international soloists.',
+      'Harjanne has made over 35 (39) solo recordings. In addition to recording the standard repertoire, Harjanne has gained a reputation for the recording of what are considered the trumpet’s most difficult works (e.g. the Zimmermann, Gruner, Nagao, Schedrin and Jolivet concertos). In addition to his recordings, he has performed in numerous productions for radio and TV.',
 
-    'Harjanne has made over 35 (39) solo recordings. In addition to recording the standard repertoire, Harjanne has gained a reputation for the recording of what are considered the trumpet’s most difficult works (e.g. the Zimmermann, Gruner, Nagao, Schedrin and Jolivet concertos). In addition to his recordings, he has performed in numerous productions for radio and TV.',
+      'Jouko Harjanne has held a number of master classes in Finland and abroad. During years 2012-2020 Harjanne was appointed as a Guest Professor of the Senzoku Gakuen College of Music. In 2012 he was nominated as a Brass Ambassador of the Lieksa city.',
+    ],
+  },
+  fi: {
+    intro: [
+      'Raumalla syntynyt Jouko Harjanne opiskeli Tampereen konservatoriossa Raimo Sarmaksen johdolla ja jatkoi opintojaan opettajinaan Henri Adelbrecht ja Timofei Dokshitser. Orkesterimuusikon uransa Harjanne aloitti vuonna 1978 Tampereen kaupunginorkesterin trumpetin varaäänenjohtajana ja jatkoi samassa orkesterissa vuoteen 1984. Vuosina 1984 – 2024 Harjanne toimi RSO:n soolotrumpetistina ja trumpettiryhmän äänenjohtajana.',
+    ],
+    bio: [
+      'Aktiivisena kamarimuusikkona hän on soittanut useissa yhtyeissä, joista keskeisimpiä ovat olleet Suomalainen Vaskiyhtye ja Solistiseitsikko Imperial.',
 
-    'Jouko Harjanne has held a number of master classes in Finland and abroad. During years 2012-2020 Harjanne was appointed as a Guest Professor of the Senzoku Gakuen College of Music. In 2012 he was nominated as a Brass Ambassador of the Lieksa city.',
-  ],
-  fi: [
-    'Raumalla syntynyt Jouko Harjanne opiskeli Tampereen konservatoriossa Raimo Sarmaksen johdolla ja jatkoi opintojaan opettajinaan Henri Adelbrecht ja Timofei Dokshitser. Orkesterimuusikon uransa Harjanne aloitti vuonna 1978 Tampereen kaupunginorkesterin trumpetin varaäänenjohtajana ja jatkoi samassa orkesterissa vuoteen 1984. Vuosina 1984 – 2024 Harjanne toimi RSO:n soolotrumpetistina ja trumpettiryhmän äänenjohtajana.',
+      'Jouko Harjanne on kansainvälisesti tunnettu ja arvostettu vaskisolisti. Maailmalla hänet luetaan aikamme trumpetistien eliittiin kuuluvaksi. Hänen solistinen uransa lähti nopeaan nousuun lukuisten kilpailumenestysten myötä. Tärkeimpiä niistä olivat toinen palkinto Prahan kevät -kilpailussa vuonna 1987 ja ensimmäinen palkinto Kansainvälisen trumpettikillan järjestämässä Ellsworth Smith -trumpettikilpailussa vuonna 1990. Harjanne on toiminut myös usean tärkeän kansainvälisen trumpettikilpailun tuomaristossa. Niistä mainittakoon mm. Munchenin ARD-kilpailu, Maurice Andre trumpettikilpailu, Prahan Kevään, Porcian (Italia), Budapestin (Unkari) sekä luonnollisesti Lieksan Vaskiviikon kansainväliset vaskikilpailut. Suomen Trumpettikillan kunniajäseneksi Harjanne kutsuttiin vuonna 1997.',
 
-    'Aktiivisena kamarimuusikkona hän on soittanut useissa yhtyeissä, joista keskeisimpiä ovat olleet Suomalainen Vaskiyhtye ja Solistiseitsikko Imperial.',
+      'Harjanteen soittoa leimaa kokonaisvaltainen musikaalisuus, jossa vaikuttavalla tavalla yhdistyvät instrumentin täydellinen hallinta, rikas ilmaisu ja silkkisen soljuva ääni. Hänen virtuoosinen instrumenttinsa käsittely nosti hänet maailman vaskimuusikoiden kermaan viimeistään yhteisessä esiintymisessä ja levytyksessä Kiri Te Kanawan kanssa Englannissa joulukuussa 1994 sekä Rodion Shchedrinin trumpettikonserton Euroopan kantaesityksessä Moskovassa syksyllä 1995. Nykyisin solistiesiintymiset vievät häntä ympäri maailmaa.',
 
-    'Jouko Harjanne on kansainvälisesti tunnettu ja arvostettu vaskisolisti. Maailmalla hänet luetaan aikamme trumpetistien eliittiin kuuluvaksi. Hänen solistinen uransa lähti nopeaan nousuun lukuisten kilpailumenestysten myötä. Tärkeimpiä niistä olivat toinen palkinto Prahan kevät -kilpailussa vuonna 1987 ja ensimmäinen palkinto Kansainvälisen trumpettikillan järjestämässä Ellsworth Smith -trumpettikilpailussa vuonna 1990. Harjanne on toiminut myös usean tärkeän kansainvälisen trumpettikilpailun tuomaristossa. Niistä mainittakoon mm. Munchenin ARD-kilpailu, Maurice Andre trumpettikilpailu, Prahan Kevään, Porcian (Italia), Budapestin (Unkari) sekä luonnollisesti Lieksan Vaskiviikon kansainväliset vaskikilpailut. Suomen Trumpettikillan kunniajäseneksi Harjanne kutsuttiin vuonna 1997.',
+      'Runsaan levytystuotantonsa, pelkästään soololevyjä on jo yli 35 kappaletta, ohella Harjanne on esiintynyt laajalti myös radiossa ja televisiossa. Levytyksillään Harjanne on saavuttanut maineen kaikkein vaikeimpien trumpettikappaleiden levyttäjänä (mm. Zimmermannin, Grunerin, Nagaon, Schedrinin ja Jolivetin konsertot). Klassisen ja modernin musiikin kattavalla laajalla repertuaarillaan Harjanne on nostanut instrumenttinsa mainetta monipuolisena soolosoittimena. Harjanne on menestyksekkäästi kantaesittänyt lukuisia suomalaisten ja kansainvälisten säveltäjien trumpettiteoksia. Monet säveltäjät ovat myös omistaneet hänelle sävellyksiään ja tunnetut kapellimestarit (mm. Kamu, Kadhize, Miller, Oramo, Panula, Salonen, Saraste, Segerstam ja Vänskä) johtaneet esiintymisiä ja levytyksiä.',
 
-    'Harjanteen soittoa leimaa kokonaisvaltainen musikaalisuus, jossa vaikuttavalla tavalla yhdistyvät instrumentin täydellinen hallinta, rikas ilmaisu ja silkkisen soljuva ääni. Hänen virtuoosinen instrumenttinsa käsittely nosti hänet maailman vaskimuusikoiden kermaan viimeistään yhteisessä esiintymisessä ja levytyksessä Kiri Te Kanawan kanssa Englannissa joulukuussa 1994 sekä Rodion Shchedrinin trumpettikonserton Euroopan kantaesityksessä Moskovassa syksyllä 1995. Nykyisin solistiesiintymiset vievät häntä ympäri maailmaa.',
+      'Harjanne pitää säännöllisesti mestarikursseja ympäri maailmaa.',
 
-    'Runsaan levytystuotantonsa, pelkästään soololevyjä on jo yli 35 kappaletta, ohella Harjanne on esiintynyt laajalti myös radiossa ja televisiossa. Levytyksillään Harjanne on saavuttanut maineen kaikkein vaikeimpien trumpettikappaleiden levyttäjänä (mm. Zimmermannin, Grunerin, Nagaon, Schedrinin ja Jolivetin konsertot). Klassisen ja modernin musiikin kattavalla laajalla repertuaarillaan Harjanne on nostanut instrumenttinsa mainetta monipuolisena soolosoittimena. Harjanne on menestyksekkäästi kantaesittänyt lukuisia suomalaisten ja kansainvälisten säveltäjien trumpettiteoksia. Monet säveltäjät ovat myös omistaneet hänelle sävellyksiään ja tunnetut kapellimestarit (mm. Kamu, Kadhize, Miller, Oramo, Panula, Salonen, Saraste, Segerstam ja Vänskä) johtaneet esiintymisiä ja levytyksiä.',
+      'Jouko Harjanne on toiminut Lieksan Vaskiviikon Taiteellisena johtajana vuodesta 1997 ja hänet on nimitetty myös Lieksan kaupungin Vaskilähettilääksi vuonna 2012. Tammikuusta 2012 vuoteen 2020 Harjanne toimi Senzoku Gakuen College of Musicin vierailevana trumpettiprofessorina.',
+    ],
+  },
+  jp: {
+    intro: [
+      '1978年から1984年まで、タンペレフィルハーモニー交響楽団の首席トランペット奏者を務めた後、1984年よりフィンランド放送交響楽団のソロ首席トランペット奏者に就任。これまでにブラスタイムクインテットやプロトヴェントゥスアンサンブルなど数多くのブラスアンサンブルや室内アンサンブルと室内楽を演奏した。',
+    ],
+    bio: [
+      'ヨウコ・ハルヤンネの国際的なキャリアは、1987年にプラハの春国際コンクールにおいて2位、1990年には国際トランペットギルド主催によるエルスワース・スミス国際トランペットコンクールにおいて1位など、多くの国際コンクールでの成功により確立された。',
 
-    'Harjanne pitää säännöllisesti mestarikursseja ympäri maailmaa.',
+      '1989年には、フィンランドで最も重要な金管楽器フェスティバルであるリエクサ・ブラスウィークよりブラスプレイヤーオブザイヤー賞を贈られる。なお現在はリエクサ・ブラスウィークの芸術監督も務めている。',
 
-    'Jouko Harjanne on toiminut Lieksan Vaskiviikon Taiteellisena johtajana vuodesta 1997 ja hänet on nimitetty myös Lieksan kaupungin Vaskilähettilääksi vuonna 2012. Tammikuusta 2012 vuoteen 2020 Harjanne toimi Senzoku Gakuen College of Musicin vierailevana trumpettiprofessorina.',
-  ],
-  jp: [
-    '1978年から1984年まで、タンペレフィルハーモニー交響楽団の首席トランペット奏者を務めた後、1984年よりフィンランド放送交響楽団のソロ首席トランペット奏者に就任。これまでにブラスタイムクインテットやプロトヴェントゥスアンサンブルなど数多くのブラスアンサンブルや室内アンサンブルと室内楽を演奏した。',
+      'ハルヤンネの国際的なソロ活動は、スカンジナビアやその他のヨーロッパの国だけでなく、ロシア、韓国、日本、アメリカなどにも及ぶ。彼はまたARDミュンヘン国際コンクール、プラハの春国際コンクール、モーリス・アンドレ国際トランペットコンクール、ボルチア国際コンクールなどでも審査員として招待された。',
 
-    'ヨウコ・ハルヤンネの国際的なキャリアは、1987年にプラハの春国際コンクールにおいて2位、1990年には国際トランペットギルド主催によるエルスワース・スミス国際トランペットコンクールにおいて1位など、多くの国際コンクールでの成功により確立された。',
+      'また1994年にイギリスで行われたコンサートにおいて、ソプラノ歌手のキリ・テ・カナワと共演したことやその録音において高く評価され、さらに1995年秋にモスクワで行われたロディオン・シチェドリンのトランペットコンチェルトの初演での成功により、さらなる高い評価を受ける。プロフェッショナルミュージックプレス誌は世界的なエリートクラスのソリストとしてハルヤンネをランクインした。',
 
-    '1989年には、フィンランドで最も重要な金管楽器フェスティバルであるリエクサ・ブラスウィークよりブラスプレイヤーオブザイヤー賞を贈られる。なお現在はリエクサ・ブラスウィークの芸術監督も務めている。',
-
-    'ハルヤンネの国際的なソロ活動は、スカンジナビアやその他のヨーロッパの国だけでなく、ロシア、韓国、日本、アメリカなどにも及ぶ。彼はまたARDミュンヘン国際コンクール、プラハの春国際コンクール、モーリス・アンドレ国際トランペットコンクール、ボルチア国際コンクールなどでも審査員として招待された。',
-
-    'また1994年にイギリスで行われたコンサートにおいて、ソプラノ歌手のキリ・テ・カナワと共演したことやその録音において高く評価され、さらに1995年秋にモスクワで行われたロディオン・シチェドリンのトランペットコンチェルトの初演での成功により、さらなる高い評価を受ける。プロフェッショナルミュージックプレス誌は世界的なエリートクラスのソリストとしてハルヤンネをランクインした。',
-
-    'ハルヤンネは数多くのレコーディングも行っている。スタンダードなレパートリーだけでなく、ツィンマーマン、グルーナー、長生、シチェドリン、ジョリベなど、トランペットにおける難度の高い作品も数多く録音し、高い評価を得ている。また録音だけでなく、テレビやラジオでも数多く出演している。また26歳の時より、ヘルシンキのシベリウスアカデミーで教鞭をとり、後進の指導にもあたっている。またフィンランドをはじめ、多くの国々でマスタークラスを行った。',
-  ],
+      'ハルヤンネは数多くのレコーディングも行っている。スタンダードなレパートリーだけでなく、ツィンマーマン、グルーナー、長生、シチェドリン、ジョリベなど、トランペットにおける難度の高い作品も数多く録音し、高い評価を得ている。また録音だけでなく、テレビやラジオでも数多く出演している。また26歳の時より、ヘルシンキのシベリウスアカデミーで教鞭をとり、後進の指導にもあたっている。またフィンランドをはじめ、多くの国々でマスタークラスを行った。',
+    ],
+  },
 };
